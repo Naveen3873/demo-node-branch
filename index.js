b358065761027e6ae0dfee4b8f646b22ede2d1f5
naveenkdoodleblue <naveen.k@doodleblue.in>
2024-08-01 12:11:20 +0530

    File write feature added

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const http = require('http');
+// const fs = require('fs');
+const fs = require('fs');
 
 http.createServer((req,rest) => {
     console.log("request handle");
 }).listen(3000);
 console.log("Server is running");
+
+
+let data = "This is a file containing a collection of books.";
+ 
+fs.writeFile("books.txt", data, (err) => {
+  if (err)
+    console.log(err);
+  else {
+    console.log("File written successfully\n");
+    console.log("The written has the following contents:");
+    console.log(fs.readFileSync("books.txt", "utf8"));
+  }
+});
