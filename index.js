1553413261720a9c854cce02b3211a4b4b75bd54
naveenkdoodleblue <naveen.k@doodleblue.in>
2024-08-01 12:03:58 +0530

    new server created

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-const fetchData = () => {
-    console.log("Data fetched...!");
-}
+const http = require('http');
 
-setTimeout(() => {
-    fetchData();
-}, 2000)
-
-
-console.log("Nodejs execute...");
-console.log("Hwllo world...");
+http.createServer((req,rest) => {
+    console.log("request handle");
+}).listen(3000);
+console.log("Server is running");
