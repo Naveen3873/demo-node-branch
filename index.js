43ef2f6e055ab20ff5b7d3822af1639125a4c989
naveenkdoodleblue <naveen.k@doodleblue.in>
2024-08-01 11:59:24 +0530

    node api calling

---
+++ index.js
@@ -0,0 +1,11 @@
+const fetchData = () => {
+    console.log("Data fetched...!");
+}
+
+setTimeout(() => {
+    fetchData();
+}, 2000)
+
+
+console.log("Nodejs execute...");
+console.log("Hwllo world...");
